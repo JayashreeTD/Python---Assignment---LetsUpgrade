8ef53a695b41192a3dc4d8a4af208cb13687c0e7
JayashreeTD <68599714+JayashreeTD@users.noreply.github.com>
2020-07-29 16:11:54 +0530

    Add files via upload

---
+++ Day 2 - Assignment.py
@@ -0,0 +1,118 @@
+
+#Day2 Assignment
+# Operator Precedence
+
+10 > 8 and 8 > 9 
+output:False
+
+a = 20
+b = 30.67
+type(a)
+output:int
+
+a = 30.78
+b = int(a)
+
+print(a)
+output:30.78
+
+j = 90
+i = float(j)
+
+print(i)
+output:90.0
+
+a = 89
+type(a)
+print(a)
+output:89
+
+k = str (a)
+print(k)
+output:89
+
+print(type(k))
+output:<class 'str'>
+
+
+#Comparison operators
+
+10 < 9
+output:False
+
+10 > 9
+output:True
+
+10 == 9
+output:False
+
+10 <= 9
+output:False
+
+10 >= 9
+output:True
+
+
+# Arithametic operation
+
+5 ** 5
+output:3125
+
+10 - 20
+output:-10
+
+10 % 3
+output:1
+
+10 // 3
+output:3
+
+10 / 3
+output:3.333333333333335
+
+10 + 4
+output:14
+
+
+# Logical operator
+
+10 >20 or 10>9
+output:True
+
+10 < 9 and 20 > M
+output:False
+
+
+# Identity operator
+sr
+x = 1.5
+y = 1.5
+x == y
+output:True
+
+a = 257
+b = 257
+a is b
+output:False
+
+a = 257
+b = 257
+a == b
+output:True
+
+
+# Membership operator
+
+str1 = "Pythonprogramming"
+"X" not in str1
+output:True
+
+str1 = "Pythonprogramming"
+"a" in str1
+output:True
+
+str1 = "Pythonprogramming"
+"X" in str1
+output:False
+
+# In[ ]:
